38696386601e81847ec946cd3006d5438a6279b5
CyberNeon <50114712+CyberNeonHD@users.noreply.github.com>
2024-01-20 04:56:45 +0100

    Bug fix of: #8
    I found that the second function had a typo it also checked "clientids.json" however, just replacing it with "other-clientids.json" didn't work out of the box, so I added an extra if statement and that seemed to work. Now #8 is fixed :D.

--- public/Start-DSClient.ps1
+++ public/Start-DSClient.ps1
@@ -168,12 +168,16 @@ function Start-DSClient {
                 $product = $script:clientids | Where-Object Product -eq $Template
 
                 if (-not $product) {
-                    # try the other ones. this is to reduce the import time when loading the module
-                    $product = Get-Content (Resolve-Path "$script:ModuleRoot\clientids.json") | ConvertFrom-Json | Where-Object Product -eq $Template
-                }
-                $ApplicationID = $product.ClientID
+                    # Try reading from other-clientids.json if not found in clientids.json
+                    if (-not $script:otherClientids) {
+                        $script:otherClientids = Get-Content (Resolve-Path "$script:ModuleRoot\other-clientids.json") | ConvertFrom-Json
+                    }
 
-                if (-not $PSBoundParameters.LargeImageKey) {
+                    $product = $script:otherClientids | Where-Object Product -eq $Template
+                }
+		$ApplicationID = $product.ClientID
+
+		if (-not $PSBoundParameters.LargeImageKey) {
                     $LargeImageKey = $product.LargeImage
                 }
                 if (-not $PSBoundParameters.LargeImageText) {
